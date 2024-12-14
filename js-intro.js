5e086dc97196352005c33ac458b77d78e4a78b67
Jual Rana <contactjual@gmail.com>
2024-12-14 20:42:53 +0600

    event practice

--- js-intro.js
+++ js-intro.js
@@ -1,1 +1,31 @@
-const idOne = document.getElementById('1')
+// document.getElementById('inner-html').innerHTML='Writing into an HTML element, using innerHTML'
+
+console.log(2 + 2 * 5);
+console.log('jual' + ' ' + 'rana');
+
+let x = 5;
+x = 7;
+x = x + x;
+console.log(x);
+
+function blockScopeTesting() {
+    let z = 5;
+    console.log('The value of z =', z);
+};
+// console.log('Out of z =', z) // cann't access though funstion is not block scope;
+blockScopeTesting();
+
+if (5 == 5) {
+    let y = 4;
+    console.log('The value of y =', y); // cannot find 'y' out of 'if' scope 
+};
+
+
+// Object access 
+const myObject = {firstName: 'Jual', lastName: 'Rana', age: 19};
+for (let [properties, value] of Object.entries(myObject)){
+    console.log(properties+ ': '+ value);
+}
+
+
+
